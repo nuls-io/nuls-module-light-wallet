3c0dfff5cd55d698228cdef42f5ec8f6ef0d14d5
wave <986887991@qq.com>
2019-07-22 10:45:45 +0800

    测试自动更新

--- src/background.js
+++ src/background.js
@@ -102,7 +102,8 @@ if (isDevelopment) {
     updateAva: {type: 3, info: '检测到新版本，正在下载……'},
     updateNotAva: {type: 4, info: '现在使用的就是最新版本，不用更新'},
   };
-  const uploadUrl = "http://file.wallet.nuls.io/download/"; // 下载地址，不加后面的**.exe http://192.168.1.119:8000/
+  //const uploadUrl = "http://file.wallet.nuls.io/download/"; // 下载地址，不加后面的**.exe http://192.168.1.119:8000/
+  const uploadUrl = "http://192.168.1.119:8000/download/"; // 下载地址，不加后面的**.exe http://192.168.1.119:8000/
   autoUpdater.setFeedURL(uploadUrl);
   autoUpdater.on('error', function (error) {
     console.log(error);
