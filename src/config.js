cc704ce55c204f6f993381b97e3fc33d0b74919c
wave <986887991@qq.com>
2019-09-06 14:18:33 +0800

    beta web B-3.2.0

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export const RUN_DEV = false;
 //ChainId和资产ID
 export const MAIN_INFO = RUN_DEV ? {chainId: 1, assetsId: 1} : {chainId: 2, assetsId: 1};
 //运行模式（true:electron窗体模式，false:浏览器模式）
-export const RUN_PATTERN = true;
+export const RUN_PATTERN = false;
 //桌面程序下载路径（main：http://file.wallet.nuls.io/download/main  test：http://file.wallet.nuls.io/download/test）background.js package 手动修改
 export const FILE_URL = RUN_DEV ? 'http://file.wallet.nuls.io/download/main' : 'http://file.wallet.nuls.io/download/test';
 //正式、测试网络的api
