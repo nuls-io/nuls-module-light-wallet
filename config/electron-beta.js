88d04c0ef4995154bd25c85fb4238510def79f83
wave <986887991@qq.com>
2019-09-18 14:52:32 +0800

    electron config

--- config/electron-beta.js
+++ config/electron-beta.js
@@ -4,21 +4,21 @@
 "use strict";
 
 //链ID
-export const API_CHAIN_ID = localStorage.hasOwnProperty('chainID') ? Number(localStorage.getItem('chainID')) : 1;
+export const API_CHAIN_ID = localStorage.hasOwnProperty('chainID') ? Number(localStorage.getItem('chainID')) : 2;
 //燃烧地址的公钥
 export const API_BURNING_ADDRESS_PUB = '000000000000000000000000000000000000000000000000000000000000000000';
 //运行环境（true:正式环境，false:测试环境）
-export const RUN_DEV = true;
+export const RUN_DEV = false;
 //ChainId和资产ID
-export const MAIN_INFO = {chainId: 1, assetsId: 1};
+export const MAIN_INFO = RUN_DEV ? {chainId: 1, assetsId: 1} : {chainId: 2, assetsId: 1};
 //运行模式（true:electron窗体模式，false:浏览器模式）
 export const RUN_PATTERN = true;
 //桌面程序下载路径
-export const FILE_URL = 'http://file.wallet.nuls.io/download/main';
+export const FILE_URL = 'http://file.wallet.nuls.io/download/test';
 //正式、测试网络的api
-export let API_URL = 'https://wallet.nuls.io/public';
+export let API_URL = 'https://beta.wallet.nuls.io/api';
 setInterval(() => {
-  API_URL = localStorage.hasOwnProperty("url") && localStorage.getItem('url') !== 'undefined' ? JSON.parse(localStorage.getItem("url")).urls : 'https://wallet.nuls.io/public';
+  API_URL = localStorage.hasOwnProperty("url") && localStorage.getItem('url') !== 'undefined' ? JSON.parse(localStorage.getItem("url")).urls : 'https://beta.wallet.nuls.io/api';
 }, 500);
 //请求最迟时间
 export const API_TIME = '8000';
@@ -110,4 +110,4 @@ export const defaultData = RUN_PATTERN ? electronData : explorerData;
 //默认服务节点地址
 export const defaultUrl = RUN_PATTERN ? electronData[0] : explorerData[0];
 //默认浏览器地址（跳转地址）
-export const explorerUrl = 'http://www.nulscan.io/';
+export const explorerUrl = 'http://beta.nulscan.io/';
